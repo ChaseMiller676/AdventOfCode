15b61775dd432e903ba9293a20ec363223b01f1d
ChaseMiller676 <millerchase676@gmail.com>
2022-12-21 22:19:57 -0500

    "Initial Commit"
    	new file:   2022/Day1/input
    	new file:   2022/Day1/mostCalories
    	new file:   2022/Day1/mostCalories.c
    	new file:   2022/Day1/topThreeCalories
    	new file:   2022/Day1/topThreeCalories.c
    	new file:   2022/Day1/topThreeCalories.c~
    	new file:   2022/Day2/input

---
+++ 2022/Day1/mostCalories.c
@@ -0,0 +1,55 @@
+/*
+Solved!
+*/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int stringToInt(char * string) {
+    int result = 0;
+
+    for (int i = 0; i < (int)(strlen(string) - 1); i++) {
+	result = result * 10 + ( string[i] - '0');
+    }
+
+    return result;
+}
+
+int main(void) {
+    FILE * pInventory;
+    char * line;
+    size_t len = 0;
+    ssize_t read;
+
+    int tempSum = 0;
+    int finalSum = 0;
+
+    pInventory = fopen("./input", "r");
+    if (pInventory == NULL) {
+	puts("File Not Found");
+	return 1;
+    }
+
+    while ((read = getline(&line, &len, pInventory)) != -1) {
+	if (strlen(line) == 1) {
+	    if (tempSum > finalSum) {
+		finalSum = tempSum;
+	    }
+
+	    tempSum = 0;
+	} else {
+	    tempSum += stringToInt(line);	    
+	}
+    }
+
+    printf("Most Calories: %i\n", finalSum);
+
+    fclose(pInventory);
+    if (line) {
+	free(line);
+    }
+
+    return 0;
+}
